e11b7a534823700a915e8d9afaba25814525fdfe
Piyush Bhatee <piyushbhatee@Piyushs-MacBook-Pro.local>
2024-07-09 16:11:18 +0530

    testing

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const message = process.env.MESSAGE || "Hey this is automated testing"
 
 // Define a route handler for the default home page
 app.get('/test', (req, res) => {
-    res.send('Hello, World!'+ " cax sz ss sd ss schuehue "+message);
+    res.send('Hello, World!'+ " cax sz ss ss schuehue "+message);
 });
 
 // Start the server
